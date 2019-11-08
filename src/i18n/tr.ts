37b8c7eb8622ad56256656f08b1ca3da87f1bf2e
Yiğit Serin <yigits@yandex.com>
2019-11-08 15:35:04 +0300

    Turkish language added.

---
+++ src/i18n/tr.ts
@@ -0,0 +1,23 @@
+export var tr = {
+    lang: 'tr',
+    translations: {
+        manup: {
+            mandatory: {
+                title: "Güncelleme Gerekli",
+                text: "Devam etmek için {{app}} güncellenmesi gerekiyor.",
+            },
+            optional: {
+                title: "Güncelleme Var",
+                text: "Yeni bir {{app}} güncellemesi var. Hemen güncelleyin."
+            },
+            maintenance: {
+                title: "{app}} Kullanım Dışı",
+                text: "{{app}} şu anda kullanılamıyor, lütfen daha sonra tekrar deneyin."
+            },
+            buttons: {
+                update: "Güncelle",
+                later: "Daha Sonra"
+            }
+        }
+    }
+}
