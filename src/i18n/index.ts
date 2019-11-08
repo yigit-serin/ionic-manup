ae9452ff592fccee0c07d0bc1c8d4b09ac30c1ed
Yiğit Serin <yigits@yandex.com>
2019-11-08 15:35:51 +0300

    Turkish language added.

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,5 +1,6 @@
 import { en } from './en';
 import { es } from './es';
 import { it } from './it';
+import { tr } from './tr';
 
-export const i18n = { en, es, it };
+export const i18n = { en, es, it, tr };
